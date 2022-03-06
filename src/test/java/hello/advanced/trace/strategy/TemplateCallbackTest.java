102b11dc308a681089479214f2d5b73cbb0b4c8e
hoon <dltjdgnsok1@naver.com>
2022-03-06 21:55:12 +0900

    템플릿 콜백 패턴을 적용하여 V4(템플릿 메서드 패턴)을 수정

---
+++ src/test/java/hello/advanced/trace/strategy/TemplateCallbackTest.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.template.Callback;
+import hello.advanced.trace.strategy.code.template.TimeLogTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateCallbackTest {
+
+    /**
+     * 팀플릿 콜백 패턴 - 익명 내부 클래스, 람다 사용
+     */
+    @Test
+    void callback() {
+        TimeLogTemplate template = new TimeLogTemplate();
+        template.execute(new Callback() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+
+        template.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
