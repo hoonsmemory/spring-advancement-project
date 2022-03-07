48f3a6be292a5df4a324577ec0175020dcbc2bd4
hoon <dltjdgnsok1@naver.com>
2022-03-07 23:19:18 +0900

    스프링 빈으로 자동 등록(Component) 예제

--- src/main/java/hello/proxy/config/AppV2Config.java
+++ src/main/java/hello/proxy/config/AppV2Config.java
@@ -10,15 +10,15 @@ import org.springframework.context.annotation.Configuration;
 public class AppV2Config {
 
     @Bean
-    public OrderControllerV2 orderControllerV1() { return new OrderControllerV2(orderServiceV1()); }
+    public OrderControllerV2 orderControllerV2() { return new OrderControllerV2(orderServiceV2()); }
 
     @Bean
-    public OrderServiceV2 orderServiceV1() {
-        return new OrderServiceV2(orderRepositoryV1());
+    public OrderServiceV2 orderServiceV2() {
+        return new OrderServiceV2(orderRepositoryV2());
     }
 
     @Bean
-    public OrderRepositoryV2 orderRepositoryV1() {
+    public OrderRepositoryV2 orderRepositoryV2() {
         return new OrderRepositoryV2();
     }
 }
