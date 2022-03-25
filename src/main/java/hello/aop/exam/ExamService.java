b16a6c20a5df202895fa661d5d77be2eca029cde
hoon <dltjdgnsok1@naver.com>
2022-03-25 20:53:23 +0900

    @annotation으로 AOP 적용

---
+++ src/main/java/hello/aop/exam/ExamService.java
@@ -0,0 +1,18 @@
+package hello.aop.exam;
+
+import hello.aop.exam.annatation.Trace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+    private final ExamRepository examRepository;
+
+    @Trace
+    public void request(String itemId) {
+        examRepository.save(itemId);
+    }
+
+}
