2c660a38723299ae46f7dddd545ae93cf728924f
hoon <dltjdgnsok1@naver.com>
2022-03-26 21:46:00 +0900

    스프링 AOP 적용 후 내부 함수 호출(문제 1 - 해결 방법 1)

--- src/main/java/hello/aop/internalcall/CallServiceV1.java
+++ src/main/java/hello/aop/internalcall/CallServiceV1.java
@@ -16,9 +16,7 @@ public class CallServiceV1 {
         log.info("CallServiceV1 setter = {}", callServiceV1.getClass());
         this.callServiceV1 = callServiceV1;
     }
-    /**
-     * 내부 호출을 할 경우 호출된 메서드는 프록시를 거치지 않는다.
-     */
+
     public void external() {
         log.info("call external");
         callServiceV1.internal(); //자기 자신을 의존관계 주입 받기
